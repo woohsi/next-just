e229fc3e0f389807176238ecc711d60ed9f077c3
Bo Hsiang <bottxiang@gmail.com>
2023-08-25 21:13:08 +0800

    update

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,45 @@
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google"
+import CredentialsProvider from "next-auth/providers/credentials"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+
+const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+    }),
+    CredentialsProvider({
+    // The name to display on the sign in form (e.g. "Sign in with...")
+    name: "Credentials",
+    // `credentials` is used to generate a form on the sign in page.
+    // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+    // e.g. domain, username, password, 2FA token, etc.
+    // You can pass any HTML attribute to the <input> tag through the object.
+    credentials: {
+      username: { label: "Username", type: "text", placeholder: "Your username" },
+      password: { label: "Password", type: "password", placeholder: "Your password" }
+    },
+    async authorize(credentials) {
+        // This is where you need to retrieve user data 
+        // to verify with credentials
+        // Docs: https://next-auth.js.org/configuration/providers/credentials
+        const user = { id: "42", name: "Dave", password: "nextauth" }
+
+        if (credentials?.username === user.name && credentials?.password === user.password) {
+            return user
+        } else {
+            return null
+        }
+    }
+  })
+  ]
+
+})
+
+export { handler as GET, handler as POST }
